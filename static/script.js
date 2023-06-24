ec23b4c584a428d37dbed0cb5503094aeb57e404
theantonyis <kostanuka@gmail.com>
2023-06-24 19:32:00 +0300

    added ajax instead of xhr in script.js

--- static/script.js
+++ static/script.js
@@ -8,25 +8,24 @@ const form = document.getElementById('form');
 const input = document.getElementById('input');
 const logout = document.getElementById('logout');
 
-let url = "https://api.weatherapi.com/v1/current.json?key=b3561c41d0154b909ff135649231806&q=Kivertsi&aqi=no";
-let xhr = new XMLHttpRequest();
-xhr.responseType = "json";
-xhr.open("GET", url);
-xhr.send();
-let weather = "";
-xhr.onload = function() {
-  weather = xhr.response;
-  if (xhr.status > 400) {
-    $("#result").html("There is some error...");
-  }
-  else {
-    console.log(weather);
-    console.log(weather.current);
-    $('#icon').attr("src", "https:" + weather.current.condition.icon);
-    $('#city').html(weather.location.country + " , " + weather.location.name);
-    $('#temp').html(weather.current.temp_c + "°C");
-  }
-}
+$.ajax({
+    url: 'https://api.weatherapi.com/v1/current.json?key=b3561c41d0154b909ff135649231806&q=Kivertsi&aqi=no',
+    type: "GET", /* or type:"GET" or type:"PUT" */
+    dataType: "json",
+    data: {
+    },
+    success: function (result) {
+        weather = result;
+        console.log(weather);
+        console.log(weather.current);
+        $('#icon').attr("src", "https:" + weather.current.condition.icon);
+        $('#city').html(weather.location.country + " , " + weather.location.name);
+        $('#temp').html(weather.current.temp_c + "°C");
+    },
+    error: function () {
+        alert("There is some error...");
+    }
+});
 
 
 logout.addEventListener('click', (e) => {
